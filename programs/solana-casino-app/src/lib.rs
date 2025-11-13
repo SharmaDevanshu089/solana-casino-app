bd8a42d9add1b423f7af21f861d8b6309d8dd4df
Devanshu Sharma <sharma.devanshu089@gmail.com>
2025-11-13 22:33:15 +0530

    Initial Commit

---
+++ programs/solana-casino-app/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("8cEcfm2JA9itFW2gLSmJ91Fq3LPMYmp4V17heZUupPJX");
+
+#[program]
+pub mod solana_casino_app {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
